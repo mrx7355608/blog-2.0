561299e15f862c2c9e0f7ac8614dcda12a54b27f
mrx7355608 <mrx7355608@gmail.com>
2025-01-05 22:08:32 +0500

    installed @tailwindcss/typography package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,20 @@
-import type { Config } from "tailwindcss";
-import daisyui from "daisyui";
+import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui';
+import daisyuiTypo from '@tailwindcss/typography';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
-  plugins: [daisyui],
+  plugins: [daisyuiTypo, daisyui],
 } satisfies Config;
