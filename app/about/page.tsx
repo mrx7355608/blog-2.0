02b111f32ba0a58acc019ff2f662c74e5616c974
mrx7355608 <mrx7355608@gmail.com>
2025-01-06 19:26:35 +0500

    created an about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,25 @@
+import Image from 'next/image';
+
 export default function About() {
-  return <div>about</div>;
+  return (
+    <div className="w-full sm:w-3/4 mx-auto p-4">
+      <h1 className="text-5xl font-black mt-8">About</h1>
+      <hr className="border-gray-900 mt-5" />
+      <p className="text-lg text-gray-300 mt-5">
+        I am Fawad Imran, a fullstack developer, with expertise in Nodejs,
+        Expressjs, MongoDB, Reactjs, Typescript, and Git technologies. Along
+        with these I am also pursuing a Bachelors degree in Computer Science.
+        <br /> <br /> I have built several fullstack projects, such as an
+        article sharing platform like medium.com, social media application,
+        realtime chatapps, multiplayer tic-tac-toe game, a truth table generator
+        and many more
+        <br /> <br /> Other than fullstack, I love programming in general. I
+        have also built some side projects related to C++, Java and Python
+        programming languages like flappy-bird game (C++, SFML), brick-breaker
+        (Java), tic-tac-toe with Minimax AI (Java), Code editor (Java), A chess
+        game (Java), My own Netcut like utility tool (Python), and the list goes
+        on
+      </p>
+    </div>
+  );
 }
